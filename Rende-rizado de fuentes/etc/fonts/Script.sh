89e064bbfba7cde60b00caab898628e1b19393a4
MakuSensei <makubexhm86@gmail.com>
2017-12-06 19:19:45 -0300

    Update Script.sh

--- Rende-rizado de fuentes/etc/fonts/Script.sh
+++ Rende-rizado de fuentes/etc/fonts/Script.sh
@@ -14,6 +14,10 @@ sudo rm /etc/fonts/conf.d/10-scale-bitmap-fonts.conf
 sleep 1
 echo aplicar configuraciones de renderizado
 sleep 1
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+sleep 1
 gsettings set org.gnome.settings-daemon.plugins.xsettings hinting "slight"
 gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
 sleep 1
