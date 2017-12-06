6dc88315bb3554784ae30df8ad454299ca796f7c
MakuSensei <makubexhm86@gmail.com>
2017-12-06 19:44:00 -0300

    Update fonts.sh

--- Rende-rizado de fuentes/etc/fonts/fonts.sh
+++ Rende-rizado de fuentes/etc/fonts/fonts.sh
@@ -18,6 +18,8 @@ sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 sleep 1
+sudo sed -i "/export/s/^#//" /etc/profile.d/freetype2.sh
+sleep 1
 gsettings set org.gnome.settings-daemon.plugins.xsettings hinting "slight"
 gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
 sleep 1
