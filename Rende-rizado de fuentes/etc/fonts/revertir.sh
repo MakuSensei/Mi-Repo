885ab9c5939f86a1527cc1c1b5eac89e15df4705
MakuSensei <makubexhm86@gmail.com>
2017-12-06 08:44:34 -0300

    Create revertir.sh

---
+++ Rende-rizado de fuentes/etc/fonts/revertir.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo Remover archivo
+sleep 1
+sudo rm /etc/fonts/local.conf
+sleep 1
+echo restaurar archivo de escalado mapa de bit
+sleep 1
+sudo cp /etc/fonts/conf.d/10-scale-bitmap-fonts.conf.bak /etc/fonts/conf.d/10-scale-bitmap-fonts.conf
+sleep 1
+echo Resetear configuraciones de fuentes
+sleep 1
+gsettings reset org.gnome.settings-daemon.plugins.xsettings hinting "slight"
+gsettings reset org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
+exit
